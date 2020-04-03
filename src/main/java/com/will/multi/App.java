3687fa54d95a25c6f84115a543d7939a18b2de74
toorop <3221254483@qq.com>
2020-04-04 00:02:53 +0800

    checkout

--- src/main/java/com/will/multi/App.java
+++ src/main/java/com/will/multi/App.java
@@ -8,10 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         System.out.println( "Hello World 2------------!" );
-=======
         System.out.println( "Hello World 1 ff----------!" );
->>>>>>> branch 'master' of https://github.com/eno-star/multi-platform.git
     }
 }
